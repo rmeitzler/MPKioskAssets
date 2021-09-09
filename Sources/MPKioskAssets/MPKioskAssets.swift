d1871b923eb61b862e7d6bf216203a5408a006f4
Richard Meitzler <richard.meitzler@gmail.com>
2021-09-09 19:40:18 -0400

    extension

--- Sources/MPKioskAssets/MPKioskAssets.swift
+++ Sources/MPKioskAssets/MPKioskAssets.swift
@@ -4,3 +4,17 @@ public struct MPKioskAssets {
     public init() {
     }
 }
+
+import SwiftUI
+@available(iOS 13.0, *)
+public extension Image {
+  init(packageResource name: String, ofType type: String) {
+      
+      guard let path = Bundle.module.path(forResource: name, ofType: type),
+              let image = UIImage(contentsOfFile: path) else {
+            self.init(name)
+            return
+        }
+        self.init(uiImage: image)
+    }
+}

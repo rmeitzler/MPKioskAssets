7921374c125e7b4ca55a66ddbbf0c3e985a6380d
Richard Meitzler <richard.meitzler@gmail.com>
2021-09-10 09:27:34 -0400

    bundle

--- Sources/MPKioskAssets/MPKioskAssets.swift
+++ Sources/MPKioskAssets/MPKioskAssets.swift
@@ -10,11 +10,17 @@ import SwiftUI
 public extension Image {
   init(packageResource name: String, ofType type: String) {
       
-      guard let path = Bundle.module.path(forResource: name, ofType: type),
-              let image = UIImage(contentsOfFile: path) else {
-            self.init(name)
-            return
-        }
+//      guard let path = Bundle.module.path(forResource: name, ofType: type),
+//        let image = UIImage(contentsOfFile: path) else {
+//            self.init(name)
+//            return
+//        }
+    
+    let image = UIImage(named: name, in: Bundle.main, compatibleWith: nil)!
         self.init(uiImage: image)
     }
+  
+//  init(kioskAsset name: String) {
+//    return
+//  }
 }
